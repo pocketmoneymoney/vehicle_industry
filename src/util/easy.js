586f7612e5260f5a21b2104bb09a9ff82bf48168
alvin <hylsd@163.com>
2018-03-13 03:25:22 -0700

    bugfix

--- src/util/easy.js
+++ src/util/easy.js
@@ -1,6 +1,6 @@
 
 function getUrlKey(name) {
-	var reg = `(^|&)${name}=([^&]*)(&|$)`
+	var reg = `(^|&)${name}=([^&]*)(&|$)`;
   	var r = window.location.search.substr(1).match(reg); 
   	if (r != null) return unescape(r[2]); 
 
